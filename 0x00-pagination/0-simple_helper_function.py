8663f813101a2ee54efe160472902810114830f1
Perfect123pct <oyewoleadekolastephen@gmail.com>
2024-04-01 15:44:59 +0000

    save

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module for pagination helper function
+Module for simple helper function
 """
 
 from typing import Tuple
@@ -19,4 +19,13 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
+
+if __name__ == "__main__":
+    res = index_range(1, 7)
+    print(type(res))
+    print(res)
+
+    res = index_range(page=3, page_size=15)
+    print(type(res))
+    print(res)
 
